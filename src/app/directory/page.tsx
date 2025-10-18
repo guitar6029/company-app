bcfb8e103b77dfc5e751003084a280eddcbc06d0
Josh <jsbusiness305@gmail.com>
2025-10-18 17:00:55 -0400

    added three new routes (about, directory, and services)

---
+++ src/app/directory/page.tsx
@@ -0,0 +1,9 @@
+import DirectoryClient from "./components/Directory/DirectoryClient";
+
+export default function DirectoryPage() {
+  return (
+    <div className="flex flex-col gap-2">
+      <DirectoryClient />
+    </div>
+  );
+}
---
+++ src/app/services/page.tsx
@@ -0,0 +1,7 @@
+export default function SerivcesPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Services Page</h1>
+    </div>
+  );
+}
