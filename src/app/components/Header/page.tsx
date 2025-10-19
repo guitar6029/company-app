d52ef4cf1c30ab43f153a83db73b7e1265ff570a
Josh <jsbusiness305@gmail.com>
2025-10-18 21:01:35 -0400

    added dark mode toggler

--- src/app/components/Header/page.tsx
+++ src/app/components/Header/page.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
-
+import { ModeToggle } from "../DarkModeToggle/Toggler";
 export default function Header() {
   return (
     <header>
       <nav className="flex flex-row items-center justify-between p-2 dark:bg-neutral-900">
-        <Link href="/">Company </Link>
         <div className="flex flex-row items-center gap-2">
+          <Link href="/">Company </Link>
           <Link href="/directory">Directory </Link>
           <Link href="/data">Data</Link>
           <Link href="/services">Services </Link>
           <Link href="/about">About </Link>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <ModeToggle />
         </div>
       </nav>
     </header>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/page";
+import { ThemeProvider } from "./components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,16 +25,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <Header />
-        <main className="flex-1 flex flex-col">
-          <div className="flex-1 flex flex-col">
-            {children}
-          </div>
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main className="flex-1 flex flex-col">
+            <div className="flex-1 flex flex-col">{children}</div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
