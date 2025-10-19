12c1cdcefee17da52fdffe9733a2011dcd28abb0
Josh <jsbusiness305@gmail.com>
2025-10-18 21:01:51 -0400

    added name and position columns with sorting

--- src/app/directory/components/Directory/DirectoryClient.tsx
+++ src/app/directory/components/Directory/DirectoryClient.tsx
@@ -2,10 +2,17 @@
 
 import { Employee } from "@/app/types/Employee";
 
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+
 interface DirectoryClientProps {
   data: Employee[];
 }
 
 export default function DirectoryCLient({ data }: DirectoryClientProps) {
-  return <div></div>;
+  return (
+    <div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
---
+++ src/app/directory/components/Directory/columns.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Employee } from "@/app/types/Employee";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export const columns: ColumnDef<Employee>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "position",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Position
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+];
