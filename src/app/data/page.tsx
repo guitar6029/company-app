561d142078f4580a5384c87a0b351b71ce25bdb4
Josh <jsbusiness305@gmail.com>
2025-10-19 17:57:29 -0400

    updated /data page

---
+++ src/app/data/page.tsx
@@ -0,0 +1,11 @@
+export default function DataPage() {
+  return (
+    <div className="relative w-full pb-[56.25%] h-0 overflow-hidden rounded-xl shadow">
+      <iframe
+        src="http://localhost:3001/d-solo/adstww2/number-of-employees-grouped-by-positions?orgId=1&from=1745096310357&to=1760907510357&timezone=browser&tab=transformations&showCategory=Panel%20options&panelId=1&__feature.dashboardSceneSolo=true"
+        className="absolute top-0 left-0 w-full h-full border-0"
+        allowFullScreen
+      />
+    </div>
+  );
+}
